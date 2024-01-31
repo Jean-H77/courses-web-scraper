b0765a0cf13173a47c1d7cac9e70979c817adce2
Jean <55899462+Jean-H77@users.noreply.github.com>
2024-01-31 00:45:21 -0800

    Remove Thread.Sleep, Replace with WebDriverWait

--- src/main/java/org/john/Client.java
+++ src/main/java/org/john/Client.java
@@ -3,6 +3,7 @@ package org.john;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,32 +12,29 @@ import java.time.Duration;
 
 public class Client {
 
-    private final WebDriver driver;
+    private final WebDriver driver = new FirefoxDriver();
     private final String URL;
 
-    public Client(WebDriver driver, String url) {
-        this.driver = driver;
+    public Client(String url) {
         URL = url;
     }
 
-    private void loadData() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    private void loadData() {
         System.setProperty("webdriver.gecko.driver","C:/geckodriver.exe");
         driver.get(URL);
 
-        WebElement iframe = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ptifrmtgtframe")));
+        WebElement iframe = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(By.id("ptifrmtgtframe")));
         driver.switchTo().frame(iframe);
 
         WebElement subject = driver.findElement(By.id("NR_SSS_SOC_NWRK_SUBJECT"));
         Select subjectDropdown = new Select(subject);
         subjectDropdown.selectByValue("COMP");
 
-        Thread.sleep(1000);
         driver.findElement(By.id("NR_SSS_SOC_NWRK_BASIC_SEARCH_PB")).click();
-        Thread.sleep(15_000); // have to find a better way to do this
+        new WebDriverWait(driver, Duration.ofSeconds(45)).until(ExpectedConditions.visibilityOfElementLocated(By.id("NR_SSS_SOC_NSEC$scroll$0")));
     }
 
-    public String getPageSource() throws InterruptedException {
+    public String getPageSource() {
         loadData();
         return driver.getPageSource();
     }
--- src/main/java/org/john/Main.java
+++ src/main/java/org/john/Main.java
@@ -1,7 +1,6 @@
 package org.john;
 
 import org.john.course.CourseSearcher;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.Scanner;
 
@@ -10,14 +9,13 @@ import static org.john.course.CourseConstants.COURSE_URL;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        Client client = new Client(new FirefoxDriver(), COURSE_URL);
+        Client client = new Client(COURSE_URL);
 
         CourseSearcher courseSearcher = new CourseSearcher();
         courseSearcher.loadCourses(client.getPageSource());
 
         while(true) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Course > ");
             String input = scanner.nextLine();
             String str = courseSearcher.generateCourseTable(input);
             System.out.println(str);
